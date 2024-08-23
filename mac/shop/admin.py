d8496c74d14b38452eaea2e88e636061b07e690d
Muskan <73654561+muskan667@users.noreply.github.com>
2024-08-23 13:11:57 +0530

    Update admin.py

--- mac/shop/admin.py
+++ mac/shop/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-
 from .models import Product,Contact,Orders,OrderUpdate
 admin.site.register(Product)
 admin.site.register(Contact)
